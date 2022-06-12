48cc21f61e7a180b838f228fa8a299dc5223b4b9
Bahadır Korumaz <bahadir.korumaz@gmail.com>
2022-06-12 05:51:07 +0300

    fix import target

--- src/components/Earth.jsx
+++ src/components/Earth.jsx
@@ -11,7 +11,7 @@ import { useGLTF } from "@react-three/drei";
 
 export default function Model(props) {
   const group = useRef();
-  const { nodes, materials } = useGLTF("./assets/oceanic_currents.gltf");
+  const { nodes, materials } = useGLTF("./public/oceanic_currents.gltf");
   return (
     <group ref={group} {...props} dispose={null}>
       <group rotation={[-Math.PI / 2, 0, 0]} scale={3}>
--- src/components/Earthquakes.jsx
+++ src/components/Earthquakes.jsx
@@ -11,7 +11,7 @@ import { useGLTF } from '@react-three/drei'
 
 export default function Model({ ...props }) {
   const group = useRef()
-  const { nodes, materials } = useGLTF('./assets/earthquakes.gltf')
+  const { nodes, materials } = useGLTF('./public/earthquakes.gltf')
   return (
     <group ref={group} {...props} dispose={null}>
       <group rotation={[-Math.PI / 2, 0, 0]}>
